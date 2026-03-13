e3887cad6f1c92501bb5c5c2891f1501b6ef6b7c
dat060603 <blinks987@gmail.com>
2026-03-13 23:01:56 +0700

    sec: remove hardcoded groq api key

--- ticket_booking/ticket_booking/settings.py
+++ ticket_booking/ticket_booking/settings.py
@@ -53,8 +53,8 @@ EMAIL_HOST_PASSWORD = 'yunn cvuv glju xebd'
 # GROQ_API_KEY='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'
 # EMAIL_HOST_USER = 'vuquochoanganh2k3@gmail.com'
 # EMAIL_HOST_PASSWORD = 'tgai qoii itcl oroc'
-GROQ_API_KEY='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'
-
+import os
+GROQ_API_KEY = os.environ.get('GROQ_API_KEY', '')
 # GROQ_API_KEY='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'
 # calery
 from celery.schedules import crontab  # Thêm import ở đây
